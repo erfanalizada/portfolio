04494f94f4fe3bdf8b720ae938dd4f10fd29d5e7
Erfan Alizada <erfanalizada6@gmail.com>
2025-06-30 11:19:35 +0200

    build try

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "826bfd50a5658ddcab5b8ba6b2dbf56f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c880666c4e40fd0c10ac73a9f4a02803",
+"flutter_bootstrap.js": "59bde53c9d45cf5a7f74a279c1c0095e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
